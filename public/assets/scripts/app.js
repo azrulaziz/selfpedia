c52ad54c9c8eed25a9a7fec936030f3111d25d78
Azrul Aziz <azrultres@gmail.com>
2018-01-01 16:37:41 +0000

    setup ajax & api

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,14 +1,45 @@
 var searchForm = document.querySelector("#searchForm");
+var searchButton = document.querySelector("#searchButton");
+var contentDiv = document.querySelector(".content");
 var inputValue;
 
-searchForm.addEventListener("keypress", appendData);
+// add event listener to search form
+searchForm.addEventListener("keypress", getValue);
 
-function appendData(e) {
+//add event listener to search button
+searchButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    inputValue = searchForm.value;
+});
+
+//function to get the value from input form and append result to a div 
+function getValue(e) {
     // e.preventDefault();
     var key = e.keyCode;
     if (key === 13) {
         inputValue = searchForm.value;
-        alert(inputValue);
-        e.preventDefault();
+        
     }
 }
+
+// function appendDiv() {
+//     contentDiv.innerHTML = "<p>" + inputValue + "</p>";   
+// }
+
+// setup ajax calls
+
+var xhr = new XMLHttpRequest();
+var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cinfo&list=search&titles=&utf8=1&ascii=1&srsearch=" + inputValue + "&srnamespace=0%7C4&srlimit=10&srwhat=text";
+xhr.open('GET', url , true);
+
+xhr.onload = function() {
+  if (this.status === 200) {
+      var data = JSON.parse(this.responseText);
+      console.log(data.query.search[0].title);
+      console.log(data.query.search[0].snippet);
+  }  
+};
+
+xhr.send();
+
+
--- selfpedia.md
+++ selfpedia.md
@@ -5,10 +5,16 @@ Objective:
 # get random articles from wiki by pressing a button 
 - create a button & open a new tab redirect to random wiki site when pressed. DONE
 
-# search something from wiki by using search form. use wiki api. 
-- get the value from the search form & save to var
-- append it to a div
-- create a search form that will append possible results from wiki into the site
+# setup the form to retrieve value from the text input 
+- get the value from the search form when user press enter      DONE
+- get the value from the search form when user press search button DONE
+- save the value to a var DONE
+- test append it to a div DONE
+
+# setup the wiki api 
+- setup ajax calls
+- get the url to get JSON data from the api
+- loop thru the JSON and output the data to the page
 
 # try to use jquery autocomplete to make the search form respond with possible results while user is typing
 
